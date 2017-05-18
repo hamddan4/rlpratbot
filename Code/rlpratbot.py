5ec96876ef5acdd83d89dfe835edc291586ca876
hamddan4 <hamddan4@gmail.com>
2017-05-19 01:11:56 +0200

    Missatge telegram bot + test mode NOW ACTIVE

--- Code/rlpratbot.py
+++ Code/rlpratbot.py
@@ -8,10 +8,13 @@ from telepot.namedtuple import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboar
 
 print('hello')
 
+test = True
+
 codes = [u'↶', u'↑', u'↷', u'✋', u'↵', u'↓', u'↳', u'-', u'+']
 dir_codes = {c: n for n, c in enumerate(codes)}
 
-ser = serial.Serial(port='COM6', baudrate=9600)
+if(not test):
+    ser = serial.Serial(port='COM8', baudrate=9600)
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
@@ -32,11 +35,11 @@ def handle(msg):
         if  msg['text'] == "/start":
             
             message = 'Hello ' + msg['from']['first_name'] + ' ' + msg['from']['last_name'] + '\n'
-            message += 'I\'m a bot that will be used for sending commands to our personal robot\n \n'
+            message += 'I\'m RAT waiter, a bot that will be used for controlling me also bring you a SODA.\n \n'
             message += 'These are the commands that I can now handle:\n'
             message += '/start welcome and information about me\n'
-            message += '/show_commands Show the keyboard to move the Robot\n'
-            message += '/hide_commands Hide the keyboard to move the Robot\n'
+            message += '/show_commands Show the keyboard to move the me\n'
+            message += '/hide_commands Hide the keyboard to move the me\n'
             
             bot.sendMessage(chat_id, message)
             
@@ -55,7 +58,8 @@ def handle(msg):
             bot.sendMessage(chat_id, message, reply_markup = resposta)
         elif msg['text'] in codes:
             bot.sendMessage(chat_id, "Command sent")
-            ser.write(bytes([dir_codes[msg['text']]]))
+            if(not test):
+                ser.write(bytes([dir_codes[msg['text']]]))
         else:
             bot.sendMessage(chat_id, "Message not recognized")
             
